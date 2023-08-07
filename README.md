9d96a710f86a62507eb2f534edc90b481d789ace
Joe <ngugijoenyaga@gmail.com>
2023-08-07 04:50:09 -0700

    elastic net model trained

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # machine-learning-full-project-v3
 trial 3 mlops
+
+
+
+MLFLOW_TRACKING_URI=https://dagshub.com/NgugiJoe/machine-learning-full-project-v3.mlflow \
+MLFLOW_TRACKING_USERNAME=NgugiJoe \
+MLFLOW_TRACKING_PASSWORD=0571859c13ac47b986ec36368b6d8e980e9b4219 \
+python script.py
+
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/NgugiJoe/machine-learning-full-project-v3.mlflow
+
+export MLFLOW_TRACKING_USERNAME=NgugiJoe 
+
+export MLFLOW_TRACKING_PASSWORD=0571859c13ac47b986ec36368b6d8e980e9b4219
---
+++ main.py
@@ -0,0 +1,55 @@
+from src.mlProject import logger
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+
+
+
+logger.info('welcome to our custom logging')
+
+STAGE_NAME = "Data Ingestion stage"
+# code from modular pipeline
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME='Model Trainer stage'
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
